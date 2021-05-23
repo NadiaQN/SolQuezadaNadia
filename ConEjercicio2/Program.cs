c9abaa63f5f5111c3bde8b1eb4ad8086c1c90e54
Nadia Quezada <quezada.nadia@gmail.com>
2021-05-23 01:13:23 -0400

    Initial commit

---
+++ ConEjercicio2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ConEjercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Declarar variables
+            string nombreComuna, strCantidadPCRPositivos, strTotalHabitantes, analizarComuna;
+            int cantidadPCRPositivos, TotalHabitantes, cantidadComunas, comunasFase4, totalPCRPositivos;
+            double porcentajeContagiados;
+
+            // Inicializar variables
+            cantidadComunas = 0;
+            comunasFase4 = 0;
+            totalPCRPositivos = 0;
+
+            Console.Write("¿Desea analizar una comuna?: (si/no) ");
+            analizarComuna = Console.ReadLine();
+
+            do
+            {   
+                // Obtener nombre comuna
+                Console.Write("Ingrese el nombre de la comuna: ");
+                nombreComuna = Console.ReadLine();
+
+                // Obtener cantidad PCR positivos
+                Console.Write("Ingrese cantidad de personas con PCR positivos: ");
+                strCantidadPCRPositivos = Console.ReadLine();
+                int.TryParse(strCantidadPCRPositivos, out cantidadPCRPositivos);
+
+                // Obtener total de habitantes en la comuna
+                Console.Write("Ingrese cantidad total de habitantes: ");
+                strTotalHabitantes = Console.ReadLine();
+                int.TryParse(strTotalHabitantes, out TotalHabitantes);
+
+                // Calcular % de habitantes contagiados
+                porcentajeContagiados = (cantidadPCRPositivos * 100) / TotalHabitantes;
+
+                // Determinar fase
+                if (porcentajeContagiados >= 80)
+                {
+                    Console.WriteLine("La comuna de {0} debe estar en Fase 1: Cuarentena", nombreComuna);
+                }
+                else if (porcentajeContagiados >= 60 && porcentajeContagiados <= 79)
+                {
+                    Console.WriteLine("La comuna de {0} debe estar en Fase 2: Transición", nombreComuna);
+                }
+                else if (porcentajeContagiados >= 50 && porcentajeContagiados <= 59)
+                {
+                    Console.WriteLine("La comuna de {0} debe estar en Fase 3: Preparación", nombreComuna);
+                }
+                else if (porcentajeContagiados >= 20 && porcentajeContagiados <= 49)
+                {
+                    Console.WriteLine("La comuna de {0} debe estar en Fase 4: Apertura Inicial", nombreComuna);
+                    comunasFase4++;
+                }
+                else
+                {
+                    Console.WriteLine("La comuna de {0} debe estar en Fase 5: Apertura Avanzada", nombreComuna);
+                }
+
+                cantidadComunas++;
+                totalPCRPositivos = totalPCRPositivos + cantidadPCRPositivos;
+
+                Console.Write("¿Desea analizar otra comuna?: (si/no) ");
+                analizarComuna = Console.ReadLine();
+            }
+            while (analizarComuna == "si");
+            if (analizarComuna == "no")
+            {
+                Console.WriteLine("Cantidad de comunas analizadas: {0} \r\nCantidad de comunas en fase 4: {1} \r\nCantidad total de PCR positivo: {2}", cantidadComunas, comunasFase4, totalPCRPositivos);
+            }
+        }
+    }
+}
---
+++ WpfEjercicio1/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfEjercicio1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.cbxServicios.Items.Add("Divorcio");
+            this.cbxServicios.Items.Add("Herencias");
+            this.cbxServicios.Items.Add("Comerciales");
+            this.cbxServicios.SelectedItem = "Divorcio";
+
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            this.txtNombreCliente.Text = string.Empty;
+            this.txtHorasEstimadas.Text = string.Empty;
+            this.lblTotal.Content = string.Empty;
+            this.lblAdelanto.Content = string.Empty;
+            this.lblMensaje.Content = string.Empty;
+            this.cbxServicios.SelectedItem = "Divorcio";
+            this.lblMensaje.Background = Brushes.Transparent;
+        }
+
+        private void btnCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            // Variables
+            string nombreCliente, tipoServicio, strHorasEstimadas;
+            double valorHora, horasEstimadas, totalServicio, adelantoServicio;
+
+            nombreCliente = this.txtNombreCliente.Text;
+            tipoServicio = this.cbxServicios.Text;
+            strHorasEstimadas = this.txtHorasEstimadas.Text;
+            valorHora = 0;
+
+            double.TryParse(strHorasEstimadas, out horasEstimadas);
+
+            // Se asigna el valor hora según cada servicio
+            switch (tipoServicio)
+            {
+                case "Divorcio":
+                    valorHora = 10000;
+                    break;
+
+                case "Herencias":
+                    valorHora = 15000;
+                    break;
+
+                case "Comerciales":
+                    valorHora = 25000;
+                    break;
+            }
+
+            // Calcular valor total del servicio
+            totalServicio = valorHora * horasEstimadas;
+
+            // Calcular adelanto a pagar
+            adelantoServicio = totalServicio * 0.4;
+
+
+            // Validar que el minimo sea de $100.000
+            if (totalServicio < 100000)
+            {
+                this.lblTotal.Content = "$" + totalServicio;
+                this.lblMensaje.Content = "El minimo para cotizar es $100.000";
+                this.lblMensaje.Background = Brushes.OrangeRed;
+
+            }
+            else
+            {
+                this.lblTotal.Content = "$" + totalServicio;
+                this.lblAdelanto.Content = "$" + adelantoServicio;
+            }
+        }
+    }
+}
